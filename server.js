bb218f9637a9c86ed3908f357a8d1f0e1ced4b65
efzyn <usenpusen1@gmail.com>
2024-08-11 01:48:19 +0700

    adding login menu or form

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors')
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
-// const mongoUri = 'mongodb+srv://efzyn:ALTRp6Dzp5eDIumw@db0.0xjiilk.mongodb.net/?retryWrites=true&w=majority&appName=db0';
 const mongoUri = process.env.MONGO_URI;
 mongoose.connect(mongoUri);
 
@@ -33,6 +37,17 @@ const bookSchema = new mongoose.Schema({
     harga: Number
 });
 const Book = mongoose.model('Book', bookSchema);
+
+// User model
+const UserSchema = new mongoose.Schema({
+    name: String,
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: String,
+});
+
+const User = mongoose.model('User', UserSchema);
 
 // Routing for HTML pages
 app.get('/', (req, res) => {
@@ -96,6 +111,62 @@ app.delete('/api/books/:id', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+// Register route
+app.post('/register', async (req, res) => {
+    const { name, username, email, password, role } = req.body;
+    
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save user
+    const user = new User({
+        name,
+        username,
+        email,
+        password: hashedPassword,
+        role
+    });
+
+    await user.save();
+    res.sendStatus(201);
+});
+
+
+// Login Route
+app.post('/login', async (req, res) => {
+    const { email, username, password } = req.body;
+
+    console.log('Login attempt with:', { email, username, password });
+
+    try {
+        const user = await User.findOne({
+            $or: [
+                { email: email },
+                { username: username }
+            ]
+        });
+
+        console.log('User found:', user);
+
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: user._id }, 'your-jwt-secret', { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
+  
 
 // Start server
 app.listen(port, () => {
